ce80601a2b4b537e1933f08b78741034e61d9f07
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-13 14:11:39 +0200

    02-component creation

--- ngxs-angular-store-example/src/app/app.module.ts
+++ ngxs-angular-store-example/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListarPostsComponent } from './listar-posts/listar-posts.component';
+import { NuevoPostComponent } from './nuevo-post/nuevo-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarPostsComponent,
+    NuevoPostComponent
   ],
   imports: [
     BrowserModule
