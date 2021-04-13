feea2a43e50987b5270970544f289cf8f68077d4
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-13 16:16:01 +0200

    09-bootstrapping the store in app.module

--- ngxs-angular-store-example/src/app/app.module.ts
+++ ngxs-angular-store-example/src/app/app.module.ts
@@ -10,13 +10,16 @@ import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { environment } from 'src/environments/environment';
+import { PostsState } from './store/posts/posts.state';
 
 @NgModule({
   declarations: [AppComponent, ListarPostsComponent, NuevoPostComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    NgxsModule.forRoot([PostsState], {
+      developmentMode: !environment.production,
+    }),
     NgxsReduxDevtoolsPluginModule.forRoot({
       disabled: environment.production,
     }),
