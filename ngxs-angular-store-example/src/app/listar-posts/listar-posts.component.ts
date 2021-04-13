f541e3b8668c791a732a6656b02ae7a2ade72b12
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-13 16:07:13 +0200

    07-modifying ListarPostsComponent

--- ngxs-angular-store-example/src/app/listar-posts/listar-posts.component.ts
+++ ngxs-angular-store-example/src/app/listar-posts/listar-posts.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Posts } from '../store/posts/posts.model';
+import { RemovePost } from '../store/posts/posts.actions';
 
 @Component({
   selector: 'app-listar-posts',
   templateUrl: './listar-posts.component.html',
-  styleUrls: ['./listar-posts.component.css']
+  styleUrls: ['./listar-posts.component.css'],
 })
 export class ListarPostsComponent implements OnInit {
+  public posts: Observable<Posts>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store) {
+    this.posts = this.store.select((state) => state.posts.posts);
   }
-
+  ngOnInit() {}
+  public removePost(id) {
+    this.store.dispatch(new RemovePost(id));
+  }
 }
